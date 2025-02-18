b7106921812a2d11cf8329d97f54160c38f9ecdd
Kutman <kutman.akun@gmail.com>
2025-02-18 13:53:01 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 
 
+import ConnectNotification from './components/connect-notification/ConnectNotification'
 import Header from './components/header/Header'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <ConnectNotification />
     </div>
   )
 }
