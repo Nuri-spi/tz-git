ab909a441d783fb6512486c0fceddc5b61572b76
Dinara <dinara.kspanovr@gmail.com>
2025-02-18 14:06:12 +0600

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@
 
 
 import Header from './components/header/Header'
+import Footer from "./components/footer/Footer.jsx";
 
 const App = () => {
   return (
     <div>
       <Header />
+
+        <Footer/>
     </div>
   )
 }
