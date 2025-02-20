c9b67c1c80bd1d8f208e012aa061dc60a0bcd011
eldiar <eldiarmatkasymov1@gmail.com>
2025-02-20 12:38:21 +0600

    Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
-
-
 import Header from './components/header/Header'
+import Menu from "./components/Menu/Menu.jsx";
 
 const App = () => {
   return (
     <div>
+        <Menu />
       <Header />
     </div>
   )
