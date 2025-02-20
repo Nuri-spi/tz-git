092577cbe1c237ecfe7f666256685c1a561b0a47
Janara <zhanaraisirailova@gmail.com>
2025-02-20 13:09:26 +0600

    janra

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import ConnectNotification from './components/connect-notification/ConnectNotification'
 import Header from './components/header/Header'
 import Footer from "./components/footer/Footer.jsx";
+import CarAd from "./components/CarAd/CarAd.jsx";
 
 const App = () => {
     return (
         <div>
             <Header/>
             <ConnectNotification/>
+            <CarAd/>
             <Footer/>
+
         </div>
     )
 }
---
+++ src/components/CarAd/CarAd.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CarAd = () => {
+    return (
+        <div className="container">
+            <h1>Janara</h1>
+        </div>
+    );
+};
+
+export default CarAd;
